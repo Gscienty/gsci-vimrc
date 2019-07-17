3ea7847c1c96e014f932ab73936a15f3322d9147
高小川 <gaoxiaochuan@hotmail.com>
2019-07-18 02:59:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,13 @@ function INSTALL_PLUG()
 {
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
+
+funcion INSTALL_EPEL()
+{
+    yum install -y epel-release
+}
+
+functon INSTALL_DEPS()
+{
+    yum install -y gcc ncurses-devel python python-devel
+}
